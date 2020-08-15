e9cc4869f6ac658b8801251f529d704975b8cb30
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-16 00:45:16 +0200

    completed dynamic dashboard equivalents & implemented npm prettifier for react numbers

--- src/views/pages/Dashboard.js
+++ src/views/pages/Dashboard.js
@@ -1,18 +1,52 @@
 import React, { useState, useEffect } from 'react';
-  import { Card, CardBody, Row, Col } from 'reactstrap';
-  import Profile from "./widgets/ProfileWidget";
-  import Flights from '../../assets/images/flights.gif';
-  import Straws from '../../assets/images/straws.gif';
-  import Bathtubs from '../../assets/images/bathtubs.gif';
-  import Car from '../../assets/images/car.gif';
-  import Coffee from '../../assets/images/coffee.gif';
-  import Plant from '../../assets/images/plant.gif';
+import { Card, CardBody, Row, Col } from 'reactstrap';
+import Profile from "./widgets/ProfileWidget";
+import Flights from '../../assets/images/flights.gif';
+import Straws from '../../assets/images/straws.gif';
+import Bathtubs from '../../assets/images/bathtubs.gif';
+import Car from '../../assets/images/car.gif';
+import Coffee from '../../assets/images/coffee.gif';
+import Plant from '../../assets/images/plant.gif';
 
-  import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import NumericLabel from 'react-pretty-numbers';
 
-  let baseURL = 'http://localhost:3003';
 
-  function Dashboard()  {
+let baseURL = 'http://localhost:3003';
+
+function Dashboard()  {
+
+  let option = {
+    'justification': 'L',
+    'locales': 'en-US',
+    'currency':false,
+    'currencyIndicator': '',
+    'percentage': false,
+    'precision': 2,
+    'wholenumber': null,
+    'commafy': true,
+    'cssClass': [''],
+    'shortFormat': true,
+    'shortFormatMinValue': 10000,
+    'shortFormatPrecision': 1,
+    'title': true
+    };
+
+    let noDecimal = {
+      'justification': 'L',
+      'locales': 'en-US',
+      'currency':false,
+      'currencyIndicator': '',
+      'percentage': false,
+      'precision': 0,
+      'wholenumber': 'floor',
+      'commafy': true,
+      'cssClass': [''],
+      'shortFormat': true,
+      'shortFormatMinValue': 10000,
+      'shortFormatPrecision': 1,
+      'title': true
+      };
 
   const { isAuthenticated, user } = useAuth0();
   const [userData, setData] = useState({});
@@ -36,6 +70,85 @@ import React, { useState, useEffect } from 'react';
       }
   } 
 
+  //Convert createdAt from DB to readable format to display in "member since"
+  let months = [
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December",
+  ];
+
+  let date = new Date(userData.createdAt);
+  let day = date.getDate();
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  let newDate = day+ ' ' +month+ ' ' +year
+
+  //Check difference between today and the date above to display in "months earth positive"
+  let now = new Date();
+
+  let diffInMonths = (now.getFullYear() * 12 + now.getMonth()) - (date.getFullYear() * 12 + date.getMonth());
+
+  // Calculate Tonnes of CO2 - ONCE PAYMENTS AND PLANS ARE SETUP THEY NEED TO BE ADDED INTO HERE FOR CORRECT CALC
+  let totalCO2 = 0;
+  let madsFootprint = 17;
+    
+  //Business
+    //Standard
+    if (userData.plan == 'Standard') {
+      let monthlyCO2 = madsFootprint/12;
+      if (diffInMonths == 0) {
+        totalCO2 = Math.round((monthlyCO2) * 100) / 100
+      } else {
+        totalCO2 = Math.round((diffInMonths * monthlyCO2) * 100) / 100
+      }
+    }
+    
+  // Calculate PLASTIC BOTTLES - ONCE PAYMENTS AND PLANS ARE SETUP THEY NEED TO BE ADDED INTO HERE FOR CORRECT CALC
+  let totalPlasticBottles = 0;
+    
+  //Business
+    //Standard
+    if (userData.plan == 'Standard') {
+      let monthlyBottles = 100;
+      if (diffInMonths == 0) {
+        totalPlasticBottles = monthlyBottles;
+      } else {
+        totalPlasticBottles = monthlyBottles * diffInMonths;
+      }
+    }
+  
+  //EQUIVALENTS
+  
+  //Calculate Flight Equivalent
+    let avgRoundTrip = 932.7;
+    let totalCO2Kg = totalCO2 * 1000;
+    let flights = totalCO2Kg/avgRoundTrip;
+  
+  //Calculate Miles Driven Equivalent
+    let avgMilesPerTonne = 2481;
+    let miles = totalCO2 * avgMilesPerTonne;
+  
+  //Calculate Plant Base Living Equivalent
+    let avgYearPlantBased = 0.8;
+    let plantBased = totalCO2 * avgYearPlantBased;
+
+  //Calculate straws equivalent
+    let avgStrawWeight = 0.0005;
+    let avgBottlesPerKg = 50;
+    let straws = (totalPlasticBottles/avgBottlesPerKg)/avgStrawWeight;
+
+  //Calculate Coffee Cups Equivalent
+    let avgCoffeeCup = 0.0004;
+    let coffeeCups = (totalPlasticBottles/avgBottlesPerKg)/avgCoffeeCup;
+  
+  //Calculate Bathtub Equivalent
+    let avgBathTub = 520;
+    let bathtubs = totalPlasticBottles/avgBathTub;
+
+
+
+
+
   useEffect(() => {
     console.log(userData)
     console.log(toggle)
@@ -54,7 +167,11 @@ import React, { useState, useEffect } from 'react';
           </Row>
           <Row>
             <Col md={6}>
-              <Profile userData={userData}/>
+              <Profile userData={userData}
+                newDate={newDate}
+                diffInMonths={diffInMonths}
+                totalCO2={totalCO2}
+                totalPlasticBottles={totalPlasticBottles}/>
             </Col>
             <Col md={3}>
               <Row>
@@ -67,7 +184,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l">
-                    <h3 className="h3" style={{ color: '#1a3066' }}><strong>29.6</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066' }}><strong><NumericLabel params={option}>{flights}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Roundtrip flights from London to Los Angeles worth of carbon saved.
                     </p>
@@ -85,7 +202,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l" style={{ color: '#1a3066', width: '100%' }}>
-                    <h3 className="h3" style={{ color: '#1a3066', width: '100%' }}><strong>68,475.6</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066', width: '100%' }}><strong><NumericLabel params={noDecimal}>{miles}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Miles driven in the average car.
                     </p>
@@ -103,7 +220,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l">
-                    <h3 className="h3" style={{ color: '#1a3066' }}><strong>22.1</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066' }}><strong><NumericLabel params={option}>{plantBased}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Years worth of carbon savings from plant-based living.
                     </p>
@@ -124,7 +241,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l">
-                    <h3 className="h3" style={{ color: '#1a3066' }}><strong>62,400</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066' }}><strong><NumericLabel params={noDecimal}>{straws}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Straws worth of plastic removed from landfill.                    </p>
                   </div>
@@ -141,7 +258,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l">
-                    <h3 className="h3" style={{ color: '#1a3066' }}><strong>7,800</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066' }}><strong><NumericLabel params={noDecimal}>{coffeeCups}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Disposable coffee cups worth of plastic saved from landfill.                    
                     </p>
@@ -159,7 +276,7 @@ import React, { useState, useEffect } from 'react';
                     aria-hidden={true}
                   />
                   <div className="m-l">
-                    <h3 className="h3" style={{ color: '#1a3066' }}><strong>3</strong></h3>
+                    <h3 className="h3" style={{ color: '#1a3066' }}><strong><NumericLabel params={option}>{bathtubs}</NumericLabel></strong></h3>
                     <p className="text-muted">
                       Bathtubs of plastic bottles rescued from the sea.
                     </p>
--- src/views/pages/widgets/ProfileWidget.js
+++ src/views/pages/widgets/ProfileWidget.js
@@ -4,69 +4,42 @@ import Sun from '../../../assets/images/sun.gif';
 import World from '../../../assets/images/world.gif';
 import Cloud from '../../../assets/images/cloud.gif';
 import Whale from '../../../assets/images/whale.gif';
+import NumericLabel from 'react-pretty-numbers';
+
 
 export default function ProfileWidget(props) {
 
-  const [calcs, setcalcs] = useState({});
+  let option = {
+    'justification': 'L',
+    'locales': 'en-US',
+    'currency':false,
+    'currencyIndicator': '',
+    'percentage': false,
+    'precision': 2,
+    'wholenumber': null,
+    'commafy': true,
+    'cssClass': [''],
+    'shortFormat': true,
+    'shortFormatMinValue': 10000,
+    'shortFormatPrecision': 1,
+    'title': true
+    };
 
-  const docalcs = () =>  {
-    
-  //Convert createdAt from DB to readable format to display in "member since"
-  }
-  let months = [
-    "January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December",
-  ];
-
-  let date = new Date(props.userData.createdAt);
-  let day = date.getDate();
-  let month = months[date.getMonth()];
-  let year = date.getFullYear();
-
-  let newDate = day+ ' ' +month+ ' ' +year
-
-  //Check difference between today and the date above to display in "months earth positive"
-  let now = new Date();
-
-  let diffInMonths = (now.getFullYear() * 12 + now.getMonth()) - (date.getFullYear() * 12 + date.getMonth());
-
-  // Calculate Tonnes of CO2 - ONCE PAYMENTS AND PLANS ARE SETUP THEY NEED TO BE ADDED INTO HERE FOR CORRECT CALC
-  let totalCO2 = 0;
-  let madsFootprint = 17;
-    
-  //Business
-    //Standard
-    if (props.userData.plan == 'Standard') {
-      let monthlyCO2 = madsFootprint/12;
-      if (diffInMonths == 0) {
-        totalCO2 = Math.round((monthlyCO2) * 100) / 100
-      } else {
-        totalCO2 = Math.round((diffInMonths * monthlyCO2) * 100) / 100
-      }
-    }
-    
-  // Calculate PLASTIC BOTTLES - ONCE PAYMENTS AND PLANS ARE SETUP THEY NEED TO BE ADDED INTO HERE FOR CORRECT CALC
-  let totalPlasticBottles = 0;
-    
-  //Business
-    //Standard
-    if (props.userData.plan == 'Standard') {
-      let monthlyBottles = 100;
-      if (diffInMonths == 0) {
-        totalPlasticBottles = Math.round((monthlyBottles) * 100) / 100
-      } else {
-        totalPlasticBottles = Math.round((monthlyBottles) * 100) / 100
-      }
-    }
-
-
-  // useEffect(() => {
-  //   if (props.userData.createdAt)  {
-  //     checkCO2()
-  //   }
-  // });
-
-
+    let noDecimal = {
+      'justification': 'L',
+      'locales': 'en-US',
+      'currency':false,
+      'currencyIndicator': '',
+      'percentage': false,
+      'precision': 0,
+      'wholenumber': null,
+      'commafy': true,
+      'cssClass': [''],
+      'shortFormat': true,
+      'shortFormatMinValue': 10000,
+      'shortFormatPrecision': 1,
+      'title': true
+      };
 
   return (
     <Card body>
@@ -76,13 +49,13 @@ export default function ProfileWidget(props) {
         </div>
         <div>
           <h2 className="h3" style={{ color: '#1a3066' }}>{props.userData.companyName}</h2>
-          <div className="h5 text-muted">Member Since: {newDate}</div>
+          <div className="h5 text-muted">Member Since: {props.newDate}</div>
           <hr />
           <Row className="text-center m-b">
               <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', paddingLeft: '50px' }} className="align-middle">
               <img src={ World } alt="World" style={{ width: 70, paddingRight: '5px' }} ></img>
 
-              <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong>{diffInMonths}</strong></h3>
+              <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong>{props.diffInMonths}</strong></h3>
               <div className="text-muted" style={{ paddingBottom: '3px' }}>Months Earth Positive</div>
               </div>
           </Row>
@@ -91,7 +64,7 @@ export default function ProfileWidget(props) {
               <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', paddingLeft: '50px' }} className="align-middle">
               <img src={ Cloud } alt="World" style={{ width: 70, paddingRight: '5px' }} ></img>
 
-              <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong>{totalCO2}</strong></h3>
+              <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong><NumericLabel params={option}>{props.totalCO2}</NumericLabel></strong></h3>
               <div className="text-muted" style={{ paddingBottom: '3px' }}>Tonnes of CO2 removed</div>
               </div>
           </Row>
@@ -100,7 +73,7 @@ export default function ProfileWidget(props) {
               <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', paddingLeft: '50px' }} className="align-middle">
               <img src={ Whale } alt="World" style={{ width: 70, paddingRight: '5px' }} ></img>
 
-               <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong>{totalPlasticBottles}</strong></h3>
+               <h3 style={{ color: '#1a3066', paddingRight: '7px', paddingLeft: '7px' }}><strong><NumericLabel params={noDecimal}>{props.totalPlasticBottles}</NumericLabel></strong></h3>
               <div className="text-muted" style={{ paddingBottom: '3px' }}>Plastic bottles prevented from entering the ocean</div>
               </div>
           </Row>
