5043e7b667a4f110ab1a375b390c2d13d71b17a3
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-16 15:23:19 +0200

    refactored uploader to use hook state and conditionally render image after upload

--- src/views/pages/Account.jsx
+++ src/views/pages/Account.jsx
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Card } from 'reactstrap';
+import { Row, Card, Button } from 'reactstrap';
 import Sun from '../../assets/images/sun.gif';
 import World from '../../assets/images/world.gif';
-import Cloud from '../../assets/images/cloud.gif';
-import Whale from '../../assets/images/whale.gif';
-import NumericLabel from 'react-pretty-numbers';
-
-import Uploader from '../../vibe/components/Uploader';
-
 
 export default function Account(props) {
+    const [imageUrl, setImageUrl] = useState();
+    const [imageAlt, setImageAlt] = useState();
+
+    //Cloudinary upload adapted from here: https://blog.logrocket.com/handling-images-with-cloudinary-in-react/
+
+    const showWidget = () => {
+    
+        let widget = window.cloudinary.createUploadWidget({ 
+            cloudName: 'dlilerh6s',
+            uploadPreset: 'pby0gwgy'}, 
+            (error, result) => {
+                if (!error && result && result.event === "success") { 
+                    console.log(result.info.url); 
+                    setImageUrl(result.info.secure_url);
+                    setImageAlt(result.info.original_filename);
+                    }
+        })
+        widget.open()
+    }
+
 
     return (
         <Card body>
-            
-            <Uploader />
-
             <div className="text-center">
                 <div className="m-b">
-                    <img src={Sun} style={{ width: 180 }} className="b-circle" alt="profile" />
+                    {
+                        imageUrl 
+                        ? <img src={imageUrl} style={{ width: 180 }} className="b-circle" alt="profile" />
+                        : <img src={Sun} style={{ width: 180 }} className="b-circle" alt="profile" />
+                    }
                 </div>
+                
             <div>
+            <Button className="btn" onClick={showWidget}> Upload Logo </Button>
             <hr />
             <Row className="text-center m-b">
                 <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', paddingLeft: '50px' }} className="align-middle">
