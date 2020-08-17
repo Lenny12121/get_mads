73e9857c4f292f9168a4c98f64c48fb2745cee8d
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 00:45:39 +0200

    changed server route

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api', getMadsController);
 
 app.use(express.static(__dirname + '/client/build/'));
 
-app.get('/', (req,res) => {
+app.get('*', (req,res) => {
   res.sendFile(__dirname + '/client/build/index.html')
 })
 
