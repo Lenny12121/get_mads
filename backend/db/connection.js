b88ecc21afe0a9018c3ba7006dc8b5edf4334932
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-17 22:34:45 +0200

    all seems to be working

---
+++ backend/db/connection.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// const connection = "mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/getmads?retryWrites=true&w=majority";
+// mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+//     .then(() => console.log("Database Connected Successfully"))
+//     .catch(err => console.log(err));
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+});
+
+mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MongoDB")
+})
+
+module.exports = mongoose
