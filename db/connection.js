f09b395789696b3dc76fb580a0d64681478a96ed
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 03:04:34 +0200

    atlas again

--- db/connection.js
+++ db/connection.js
@@ -1,63 +1,63 @@
-const mongoose = require('mongoose');
-
-// const connection = "mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/getmads?retryWrites=true&w=majority";
-// mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-//     .then(() => console.log("Database Connected Successfully"))
-//     .catch(err => console.log(err));
-
-require('dotenv').config();
-
-// const MongoClient = require("mongodb").MongoClient;
-// const client = await new MongoClient(process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads',{ useNewUrlParser: true});
-// client.connect();
-// mongoose.connection.once('open', () => { console.log('MongoDB Connected'); });
-// mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err); }); 
-
 // const mongoose = require('mongoose');
-// const MongoClient = require('mongodb').MongoClient;
 
+// // const connection = "mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/getmads?retryWrites=true&w=majority";
+// // mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+// //     .then(() => console.log("Database Connected Successfully"))
+// //     .catch(err => console.log(err));
 
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
-// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
-
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// mongoose.connection.once('open', ()=>{
-//     console.log('connected to mongoose...');
-// });
-
-
-
-
-
-module.exports = mongoose;
-
-// const MongoClient = require("mongodb").MongoClient;
 // require('dotenv').config();
 
+// // const MongoClient = require("mongodb").MongoClient;
+// // const client = await new MongoClient(process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads',{ useNewUrlParser: true});
+// // client.connect();
+// // mongoose.connection.once('open', () => { console.log('MongoDB Connected'); });
+// // mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err); }); 
 
-// const dbConnectionUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads';
+// // const mongoose = require('mongoose');
+// // const MongoClient = require('mongodb').MongoClient;
 
-// function initialize(
-//     dbName,
-//     dbCollectionName,
-//     successCallback,
-//     failureCallback
-// ) {
-//     MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
-//         if (err) {
-//             console.log(`[MongoDB connection] ERROR: ${err}`);
-//             failureCallback(err); // this should be "caught" by the calling function
-//         } else {
-//             const dbObject = dbInstance.db(dbName);
-//             const dbCollection = dbObject.collection(dbCollectionName);
-//             console.log("[MongoDB connection] SUCCESS");
 
-//             successCallback(dbCollection);
-//         }
-//     });
-// }
+// // const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
+// // mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+// // mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-// module.exports = {
-//     initialize
-// };
+// // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// // mongoose.connection.once('open', ()=>{
+// //     console.log('connected to mongoose...');
+// // });
+
+
+
+
+
+// module.exports = mongoose;
+
+const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config();
+
+
+const dbConnectionUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads';
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+module.exports = {
+    initialize
+};
