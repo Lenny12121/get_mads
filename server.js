1ffc700e42b0147e2a6d857aa7748362ca38c75a
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 13:53:49 +0200

    more atlas

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ require('dotenv').config();
 
 // require('dotenv').config();
 
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
-// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// mongoose.connection.once('open', ()=>{
-//     console.log('connected to mongoose...');
-// });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...');
+});
 
 // const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads';
 // mongoose.connect(uri, {
