943ea4f95303aa68eba45a52047a7d0ad5ba4b0f
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 13:43:02 +0200

    still testing atlas

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 // const connection = "mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/GetMads?retryWrites=true&w=majority";
 // mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 //     .then(() => console.log("Database Connected Successfully"))
 //     .catch(err => console.log(err));
 
-// require('dotenv').config();
+require('dotenv').config();
 
 // // const MongoClient = require("mongodb").MongoClient;
 // // const client = await new MongoClient(process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads',{ useNewUrlParser: true});
@@ -17,48 +17,48 @@
 // // const MongoClient = require('mongodb').MongoClient;
 
 
-// // const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
-// const MONGODB_URI = 'mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/GetMads?retryWrites=true&w=majority'
-// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
+const MONGODB_URI = 'mongodb+srv://Lenny:Crabtree40@getmads.8xgth.azure.mongodb.net/GetMads?retryWrites=true&w=majority'
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// mongoose.connection.once('open', ()=>{
-//     console.log('connected to mongoose...');
-// });
-
-
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...');
+});
 
 
 
-// module.exports = mongoose;
-
-const MongoClient = require("mongodb").MongoClient;
-require('dotenv').config();
 
 
-const dbConnectionUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads';
+module.exports = mongoose;
 
-function initialize(
-    dbName,
-    dbCollectionName,
-    successCallback,
-    failureCallback
-) {
-    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
-        if (err) {
-            console.log(`[MongoDB connection] ERROR: ${err}`);
-            failureCallback(err); // this should be "caught" by the calling function
-        } else {
-            const dbObject = dbInstance.db(dbName);
-            const dbCollection = dbObject.collection(dbCollectionName);
-            console.log("[MongoDB connection] SUCCESS");
+// const MongoClient = require("mongodb").MongoClient;
+// require('dotenv').config();
 
-            successCallback(dbCollection);
-        }
-    });
-}
 
-module.exports = {
-    initialize
-};
+// const dbConnectionUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads';
+
+// function initialize(
+//     dbName,
+//     dbCollectionName,
+//     successCallback,
+//     failureCallback
+// ) {
+//     MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+//         if (err) {
+//             console.log(`[MongoDB connection] ERROR: ${err}`);
+//             failureCallback(err); // this should be "caught" by the calling function
+//         } else {
+//             const dbObject = dbInstance.db(dbName);
+//             const dbCollection = dbObject.collection(dbCollectionName);
+//             console.log("[MongoDB connection] SUCCESS");
+
+//             successCallback(dbCollection);
+//         }
+//     });
+// }
+
+// module.exports = {
+//     initialize
+// };
