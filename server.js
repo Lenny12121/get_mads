722833c6531b53dd95012917403f4037c26fd1f7
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 02:51:05 +0200

    changes to atlas

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3003;
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 // const cors = require('cors');
 // require('./database');
 
@@ -37,9 +39,9 @@ const mongoose = require('mongoose');
 // })
 // .catch(err => console.log(err))
 
-const db = require("./db/connection");
-const dbName = "GetMads";
-const collectionName = "users";
+// const db = require("./db/connection");
+// const dbName = "GetMads";
+// const collectionName = "users";
 
 //middleware
 app.use(express.json());
@@ -62,12 +64,12 @@ app.use(express.static(path.join(__dirname, "/public/build")));
 // app.use(cors(corsOptions));
 
 
-db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
-  // get all items
-  dbCollection.find().toArray(function(err, result) {
-      if (err) throw err;
-        console.log(result);
-  });
+// db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+//   // get all items
+//   dbCollection.find().toArray(function(err, result) {
+//       if (err) throw err;
+//         console.log(result);
+//   });
 
 //controllers
 const getMadsController = require('./controllers/getmads.js');
@@ -76,9 +78,9 @@ app.use('/api', getMadsController);
 //     res.sendFile(path.join(__dirname, "../build"));
 // });
 
-}, function(err) { // failureCallback
-  throw (err);
-});
+// }, function(err) { // failureCallback
+//   throw (err);
+// });
 
 app.use(express.static(__dirname + '/client/build/'));
 
