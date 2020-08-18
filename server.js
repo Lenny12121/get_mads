c1acec3af50004e95aa7513293ab675719f96a03
“[Lenny <felixleemann@Felixs-MacBook-Pro.local>
2020-08-18 02:00:00 +0200

    changed it back

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ const mongoose = require('mongoose');
 //     console.log('connected to mongoose...')
 // });
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/getmads'
+// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...');
-});
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connection.once('open', ()=>{
+//     console.log('connected to mongoose...');
+// });
 
 //middleware
 app.use(express.json());
